4462c3e1d28a694a94002d7095757132b416300b
Ben Potter <ppybcp@nottingham.ac.uk>
2021-02-09 23:04:58 +0000

    Just still testing debugging and vs code.

--- Draft.py
+++ Draft.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import scipy.constants as spc
+import os
 
 mpl.rcParams['text.usetex'] = True 
 mpl.rcParams['text.latex.preamble'] = [r'\usepackage[cm]{sfmath}', r'\usepackage{braket}']
@@ -10,8 +11,6 @@ mpl.rcParams['font.sans-serif'] = 'cm'
 mpl.rcParams['font.size'] = 27
 mpl.rcParams["savefig.format"] = 'eps'
 
+# Use os.getcwd() to get the folder you're in atm
+
 grid = np.zeros((9,9))
-t = np.linspace(0,4,100)
-y = np.sin(2*np.pi*t)
-plt.plot(t,y)
-plt.show()
