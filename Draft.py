cb87f52966c10a006e44bc665fd0c8cb30fd011c
Ben Potter <ppybcp@nottingham.ac.uk>
2021-02-12 20:45:39 +0000

    Initial draft of the programming
    Co-authored-by: Alex Heaton <alexheaton365@gmail.com>

--- Draft.py
+++ Draft.py
@@ -13,10 +13,77 @@ mpl.rcParams["savefig.format"] = 'eps'
 
 # Use os.getcwd() to get the folder you're in atm
 
-grid = np.zeros((9,9))
-t = np.linspace(0,3,100)
-y = np.sin(2*np.pi*t)
-plt.plot(t,y)
-plt.xlabel(r'Time $t$')
-plt.ylabel(r'$\sin(2 \pi t)$')
-plt.show()
+
+
+def exchange(J,s1,s2):
+    if s1 == s2:
+        spprod = 0.25*spc.hbar**2
+    else:
+        spprod = -0.75*spc.hbar**2
+    Eex=-J*spprod
+    return Eex
+
+def anis(k,th):
+    Ek = -k*(np.sin(th))**2
+    return Ek
+
+def diff(Eb,Ea):
+    delE = Eb-Ea
+    return delE
+
+def Prob(dE,T):
+    P=np.exp(-dE/(spc.k*T))
+    test = np.random.random()
+    if test < P:
+        return 1
+    else:
+        return 0
+
+J=50
+k=1
+T = 1
+limit = 9
+lol = limit**2
+
+anggrid = 2*np.pi*np.random.random((limit,limit))
+spingrid = np.random.randint(0,2, size=(limit,limit)) - 0.5
+
+locr = np.random.randint(1,limit-1)
+locc = np.random.randint(1,limit-1)
+
+exchangelist = np.zeros((4))
+for i in range(0,4):
+    if (i % 2) == 0:
+        exchangelist[i] = exchange(J, spingrid[locr,locc], spingrid[locr+(i-1),locc])
+    else:
+        exchangelist[i] = exchange(J, spingrid[locr,locc], spingrid[locr,locc+(i-2)])
+
+an = anis(k, anggrid[locr,locc])
+Es = an + np.sum(exchangelist)
+
+randturn = (2*np.pi*np.random.random()) % (2*np.pi)
+
+
+exchangelist2 = np.zeros((4))
+for i in range(0,4):
+    if (i % 2) == 0:
+        exchangelist2[i] = exchange(J, spingrid[locr,locc], spingrid[locr+(i-1),locc])
+    else:
+        exchangelist2[i] = exchange(J, spingrid[locr,locc], spingrid[locr,locc+(i-2)])
+
+an2 = anis(k, anggrid[locr,locc])
+Ef = an2 + np.sum(exchangelist2)
+
+delE = diff(Es,Ef)
+
+turn = Prob(delE,T)
+
+if turn == 1:
+    anggrid[locr,locc] = anggrid[locr,locc] + randturn
+else:
+    pass
+
+
+
+
+
